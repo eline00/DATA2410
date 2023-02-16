c530b1693120f585d184770de8068a9082f4f86f
eline00 <90403068+eline00@users.noreply.github.com>
2023-02-16 17:30:51 +0100

    first commit

---
+++ Lab2/server.py
@@ -0,0 +1,93 @@
+"""
+Server side: it simultaneously handle multiple clients
+and broadcast when a client new client joins or a client
+sends a message.
+"""
+from socket import *
+import _thread as thread
+from threading import activeCount
+import time
+import sys
+
+
+#this is too keep all the newly joined connections! 
+all_client_connections = []
+
+def now():
+	"""
+	returns the time of day
+	"""
+	return time.ctime(time.time())
+
+def handleClient(connection, addr):
+	"""
+	a client handler function 
+	"""
+	
+	all_client_connections.append(connection)
+ 
+	message = f"[NEW CONNECTION] {addr} connected."
+	broadcast(connection, message)
+	connection.send("Welcome!".encode())
+	
+	
+	try:
+		while True:
+			message = connection.recv(2048).decode()
+			print (now() + " " +  str(addr) + "#  ", message)
+   
+			if message == "exit" or not message:
+				print(f"{addr} har left the server.")
+				broadcast(connection, f"{addr} har left the server.")
+				break
+			
+			broadcast(connection, f"[{addr}] {message}")
+			connection.send("Message recieved".encode())
+	except:
+		connection.close()
+		all_client_connections.remove(connection)
+		
+
+def broadcast(connection, message):
+	print ("Broadcasting")
+	
+	for i in all_client_connections:
+		if (i != connection):
+			i.send(message.encode())
+	
+
+def main():
+	"""
+	creates a server socket, listens for new connections,
+	and spawns a new thread whenever a new connection join
+	"""
+	serverPort = 12000
+	serverSocket = socket(AF_INET,SOCK_STREAM)
+	try:
+		# Use the bind function wisely!
+		### Write your code here ###
+		serverSocket.bind(('', serverPort))
+		### Your code ends here ###
+		
+	except: 
+		print("Bind failed. Error : ")
+		sys.exit()
+	serverSocket.listen(1)
+	print(f"[LISTENING] server is listening on {gethostbyname(gethostname())}")
+	print ('The server is ready to receive')
+	while True:
+		### Write your code here ###
+		connectionSocket, addr =   serverSocket.accept()
+		### You code ends here ###
+		 
+		print('Server connected by ', addr) 
+		print('at ', now())
+		thread.start_new_thread(handleClient, (connectionSocket,addr)) 
+	serverSocket.close()
+
+if __name__ == '__main__':
+	main()
+	
+
+
+
