e208b3c4ea8a5612ee8db96afdf95f04cbff86c4
eline00 <90403068+eline00@users.noreply.github.com>
2023-03-02 13:22:29 +0100

    qpdated the server and clinet so that it works

--- Oblig1/server.py
+++ Oblig1/server.py
@@ -9,7 +9,7 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 HOST = '127.0.0.1'
 PORT = 6789
 
-serverSocket.bind((HOST, PORT))
+serverSocket.bind(('', PORT))
 serverSocket.listen(1)
 print('Ready to serve...')
 
@@ -19,33 +19,34 @@ while True:
 	connectionSocket, addr = serverSocket.accept()
 	
 	try:
-		message = connectionSocket.recv(1024).decode()
+		message = connectionSocket.recv(2048).decode()
 		filename = message.split()[1][1:]
 		with open(filename, "rb") as f:
 			outputdata = f.read()
 
 		#Send one HTTP header line into socket
 		connectionSocket.send('HTTP/1.1 200 OK\r\n'.encode())
-		connectionSocket.send('Content-Type: text/html\r\n'.encode())
-		connectionSocket.send(('Content-Length: %d\r\n' % len(outputdata)).encode())
 		connectionSocket.send('\r\n'.encode())
 
 		#Send the content of the requested file to the client 
+		
 		for i in range(0, len(outputdata)):
-			connectionSocket.send(outputdata[i:i+1])
+			connectionSocket.send(outputdata[i:i+1]) 
+
+		#connectionSocket.close()
 
 
 	except IOError:
-     	#Close client socket
+	 	#Close client socket
   
 		#Send response message for file not found
 		connectionSocket.send('HTTP/1.1 404 Not Found\r\n'.encode())
-		connectionSocket.send('Content-Type: text/html\r\n'.encode())
 		connectionSocket.send('\r\n'.encode())
 		connectionSocket.send('<html><body><h1>404 Not Found</h1></body></html>\r\n'.encode())
 
+	finally:
 		connectionSocket.close()
 		
 
 serverSocket.close()
-sys.exit() #Terminate the program after sending the corresponding data
+
--- Oblig1/webclient.py
+++ Oblig1/webclient.py
@@ -5,25 +5,33 @@ import sys
 clientSocket = socket(AF_INET, SOCK_STREAM)
 
 if len(sys.argv) != 4:
-    print("Usage: client.py <server_host> <server_port> <filename>")
-    sys.exit(1)
-    
+	print("Usage: webclient.py <server_host> <server_port> <filename>")
+	sys.exit(1)
+	
 HOST = sys.argv[1]
 PORT = int(sys.argv[2])
 filename = sys.argv[3]
+
 
 # connecting to the server
 clientSocket.connect((HOST, PORT))
 
 # sending HTTP GET request to server
-message = "GET /" + filename + "HTTP/1.1\r\nHost: " + HOST + "\r\n\r\n"
-clientSocket.send(message.encode())
+request = "GET /" + filename + " HTTP/1.1\r\nHost: " + HOST + "\r\n\r\n"
+clientSocket.send(request.encode())
 
+message = b''
+while True:
+	data = clientSocket.recv(2048)
+	if not data:
+		break
+	message += data
+	
 # response from server
-response = clientSocket.recv(1024)
+# response = clientSocket.recv(2048)
 
 # printing response
-print(response.decode())
+print(message.decode())
 
 clientSocket.close()
 sys.exit()
